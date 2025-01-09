b1a0c4347b4ff3bda0ed296993c200e5b08f665a
pejuangbobo <kelasemir@gmail.com>
2025-01-09 09:33:57 +0700

    Add files via upload

---
+++ jsbuathalamanutama.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("Halaman utama siap!");
+  // Anda dapat menambahkan interaktivitas lebih lanjut di sini jika diperlukan.
+});
